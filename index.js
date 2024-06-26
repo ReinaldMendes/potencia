b3b186c11c239e5a8edfb823816acf0295d279be
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-06-26 11:37:41 -0300

    potencia

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1. Pergunte um número para base;
+// 2. Pergunte também o expoente.
+// 3. Calcule a potência apenas utilizando do operador matemático + (soma)
+let resultado;
+const prompt = require("prompt-sync")();
+let numero = prompt("Digite um número ");
+let expoente = prompt("Digite o expoente ");
+for (let i = 0; i < expoente; i++) {
+  resultado = numero + i;
+}
+console.log(resultado);
