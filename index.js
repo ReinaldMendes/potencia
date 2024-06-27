c653cb6451c289a0813e61e1cb937a32d3149c85
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-06-27 08:10:03 -0300

    exponencial

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const prompt = require("prompt-sync")();
 let numero = prompt("Digite um número ");
 let expoente = prompt("Digite o expoente ");
 for (let i = 0; i < expoente; i++) {
-  resultado = numero + i;
+  resultado = numero ** expoente;
 }
 console.log(resultado);
